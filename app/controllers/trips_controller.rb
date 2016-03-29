e44f1229438f30ddf71157b3a0b1f681e7b17ab4
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-03-29 09:51:12 +0800

    delete,edit, add trip working

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -19,9 +19,31 @@ class TripsController < ApplicationController
   end
 
   def update
+    @trip = Trip.update(params[:id], trip_params)
+    respond_to do |format|
+      if @trip.save
+        format.json { render json: @trip }
+      else
+        format.json { render json: @trip.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    Trip.destroy(params[:id])
+    respond_to do |format|
+      # format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    def trip_params
+      params.require(:trip).permit(:location, :start_date, :end_date)
+    end
+
+    # def editedtrip_params
+    #   params.require(:editedtrip).permit(:location, :start_date, :end_date)
+    # end
 
 end
--- app/views/trips/index.jbuilder
+++ app/views/trips/index.jbuilder
@@ -1,6 +1,6 @@
 json.array! @trips do |trip|
   json.id trip.id
-  json.title trip.location
-  json.content trip.start_date
-  json.category trip.end_date
+  json.location trip.location
+  json.start_date trip.start_date
+  json.end_date trip.end_date
 end
