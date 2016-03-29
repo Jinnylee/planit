a755e6e7c27effd428ee772b9182199c5218ee4b
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-03-29 23:08:25 +0800

    created members controller

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,15 @@
+class MembersController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   resources :trips, only: [:index, :show, :create, :update, :destroy] do
+    resources :members, only: [:index, :create, :update, :destroy]
     resources :flights, only: [:index, :create, :update, :destroy]
     resources :activities, only: [:index, :create, :update, :destroy]
     resources :expenses, only: [:index, :create, :update, :destroy]
