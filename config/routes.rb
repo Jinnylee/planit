ff675a54bbf71d13888016ee78271341e1f4a4e6
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-04-01 15:08:27 +0800

    editing expenses working, nav routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
 
   resources :trips, only: [:index, :show, :create, :update, :destroy] do
     resources :members, only: [:index, :create, :update, :destroy]
-    resources :flights, only: [:index, :create, :update, :destroy]
-    resources :activities, only: [:index, :create, :update, :destroy]
-    resources :expenses, only: [:index, :create, :update, :destroy]
-    resources :accommodations, only: [:index, :create, :update, :destroy]
+    resources :flights, only: [:index, :show, :create, :update, :destroy]
+    resources :activities, only: [:index, :show, :create, :update, :destroy]
+    resources :expenses, only: [:index, :show, :create, :update, :destroy]
+    resources :accommodations, only: [:index, :show, :create, :update, :destroy]
     resources :packing, only: [:index, :create, :update, :destroy]
   end
 end
