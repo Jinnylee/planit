4a6109bb41aacccae8958a2c535550429d4cb262
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-04-04 11:18:06 +0800

    packings get, add, delete

--- app/controllers/packings_controller.rb
+++ app/controllers/packings_controller.rb
@@ -1,22 +1,22 @@
 class PackingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_trip, only: [:create, :update, :destroy]
 
   def index
-    @packings = Packing.all
+    @packings = Packing.where(trip_id: params[:trip_id])
     respond_to do |format|
       format.json { render json: @packings }
     end
   end
 
   def create
-    @packing = Packing.new(packing_params)
-    respond_to do |format|
+    @packing = @trip.packings.new(item: params[:packing])
       if @packing.save
-        format.json { render json: @packing }
+        render json: @packing
       else
-        format.json { render json: @packing.errors, status: :unprocessable_entity }
+        render json: @packing.errors, status: :unprocessable_entity
       end
-    end
+
   end
 
   def update
@@ -31,28 +31,18 @@ class PackingsController < ApplicationController
   end
 
   def destroy
-    Userpacking.destroy_all(packing_id: params[:id])
-    Trip.destroy(params[:id])
+    Packing.destroy(params[:id])
     respond_to do |format|
-      # format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
-  def get_users
-    @users = UserTrip.includes(:user).where(trip_id: params[:trip_id]).map(&:user)
-    respond_to do |format|
-      format.json { render json: @users }
-    end
-  end
-
   private
-    def packing_params
-      params.require(:packing)
+    def set_trip
+      @trip = Trip.find_by(id: params[:trip_id])
+      if @trip.nil?
+        render json: {message: "Can't find trip"}, status: 404
+      end
     end
-
-    # def editedpacking_params
-    #   params.require(:editedtrip).permit(:location, :start_date, :end_date)
-    # end
 
 end
