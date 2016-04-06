43cc98b72c1843e3ccdc0bdc4ab9eee19b376660
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-04-06 12:22:46 +0800

    order_by accommodation

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -3,7 +3,7 @@ class AccommodationsController < ApplicationController
   before_action :set_trip, only: [:create]
 
   def index
-    @accommodations = Accommodation.includes(:accommodation_splits).where(trip_id: params[:trip_id])
+    @accommodations = Accommodation.includes(:accommodation_splits).where(trip_id: params[:trip_id]).order(check_in: :asc)
     respond_to do |format|
       format.json { render json: @accommodations.as_json(include: { accommodation_splits: { include: :user }, user: {} })}
     end
