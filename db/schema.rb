bc37edfb7b7f33610cd3e34335771b37364691a6
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-04-06 13:24:53 +0800

    changed accommodations table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404142440) do
+ActiveRecord::Schema.define(version: 20160406050501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160404142440) do
     t.float    "price"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "link"
   end
 
   create_table "activities", force: :cascade do |t|
